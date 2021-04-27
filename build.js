e48f04c342007252d40c9f7ec309e2b7f8e9f74f
Subhajit Sahu <wolfram77@gmail.com>
2021-04-27 23:52:53 +0530

    :skunk: w/ build script

---
+++ build.js
@@ -0,0 +1,21 @@
+const cp = require('child_process');
+
+const URL = 'https://github.com/cppf/nvgraph.sh/releases/download/0.0.1/nvgraph';
+
+
+
+
+function fetchSuper(url) {
+  cp.execSync(`wget -nv ${url}`);
+}
+
+function makeExec() {
+  cp.execSync(`chmod +x nvgraph`);
+  cp.execSync(`chmod +x index.sh`);
+}
+
+function main() {
+  fetchSuper(URL);
+  makeExec();
+}
+main();
